53c8e32de49cd7e41e5d1cde73e45cb97413802a
Zenon Perise <perise@subshell.com>
2021-04-08 17:03:46 +0200

    Exporter arreglat i funcionant

--- src/exporter.py
+++ src/exporter.py
@@ -1,12 +1,10 @@
 import csv
-import pandas as pd
-from main import df
-
-print("Importem llibreries per carragar les variables")
 
 
+def writeResult(df):
+    print("Importem llibreries per carregar les variables")
 
-with open('output.csv', 'w+') as f:
-    f.write(df)
-    
-print("S'ha creat un arxiu csv output.csv amb les dades obtingudes")
+    with open('output.csv', 'w+') as f:
+        f.write(df)
+
+    print("S'ha creat un arxiu csv output.csv amb les dades obtingudes")
--- src/main.py
+++ src/main.py
@@ -1,28 +1,34 @@
 
 from pandas import DataFrame
 import time
+from exporter import writeResult
+from importer import readPage
 
-print("Importem llibreria pandas per carragar les variables")
-print("Importem llibreria time per afegir delay i per obtenir dades sense saturar el servidor)
+print("Importem llibreria pandas per carregar les variables")
+print("Importem llibreria time per afegir delay i per obtenir dades sense saturar el servidor")
 pages = 600
 allData = []
 for page in range(pages):
     time.sleep(1)
+    print("Llegint pàgina {}".format(page))
     for entry in readPage(page):
         allData.append(entry)
-       
+
 page = list(map(lambda x: x.page, allData))
 url = list(map(lambda x: x.url, allData))
 summary = list(map(lambda x: x.summary, allData))
 votes = list(map(lambda x: x.votes, allData))
-published_date = list(map(lambda x: x.date1, allData))
-send_date = list(map(lambda x: x.date2, allData))
+published_date = list(map(lambda x: x.published_date, allData))
+send_date = list(map(lambda x: x.send_date, allData))
 category = list(map(lambda x: x.category, allData))
 
 data = [page, url, summary, votes, published_date, send_date, category]
 
 df = DataFrame(data).transpose()
 
-df.columns = ["page", "url", "summary", "votes", "published_date", "send_date", "category"]
+df.columns = ["page", "url", "summary", "votes",
+              "published_date", "send_date", "category"]
 
 print("Variables carregades i amb encapçalat")
+
+writeResult(df)
