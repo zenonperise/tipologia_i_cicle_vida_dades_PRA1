8f7d596817639d5fcae0b8aedd51482b3b688921
Adria Vidal <adria.vdp@gmail.com>
2021-04-08 16:43:15 +0200

    nou 2

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 
-from importer import readPage
 from pandas import DataFrame
 import time
 
-pages = 3
+print("Importem llibreria pandas per carragar les variables")
+print("Importem llibreria time per afegir delay i per obtenir dades sense saturar el servidor)
+pages = 600
 allData = []
 for page in range(pages):
-    time.sleep(20)
+    time.sleep(1)
     for entry in readPage(page):
         allData.append(entry)
        
@@ -14,16 +15,14 @@ page = list(map(lambda x: x.page, allData))
 url = list(map(lambda x: x.url, allData))
 summary = list(map(lambda x: x.summary, allData))
 votes = list(map(lambda x: x.votes, allData))
-date1 = list(map(lambda x: x.date1, allData))
-date2 = list(map(lambda x: x.date2, allData))
+published_date = list(map(lambda x: x.date1, allData))
+send_date = list(map(lambda x: x.date2, allData))
 category = list(map(lambda x: x.category, allData))
 
-data = [page, url, summary, votes, date1, date2, category]
+data = [page, url, summary, votes, published_date, send_date, category]
 
 df = DataFrame(data).transpose()
 
-df.columns = ["page", "url", "summary", "votes", "date1", "date2", "category"]
+df.columns = ["page", "url", "summary", "votes", "published_date", "send_date", "category"]
 
-df= df.to_csv(index=False)
-with open('output.csv', 'w+') as f:
-    f.write(df)
+print("Variables carregades i amb encapçalat")
