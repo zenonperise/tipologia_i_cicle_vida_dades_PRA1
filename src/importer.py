8845905db1ef7ffb72202d6574fff2e7759094d0
Adria Vidal <adria.vdp@gmail.com>
2021-04-08 16:39:53 +0200

    nou

--- src/importer.py
+++ src/importer.py
@@ -1,8 +1,8 @@
 import requests
-
 from bs4 import BeautifulSoup
 
-
+print("Importem llibreria request per demanar les dades")
+print("importem la llibreria BeatifulSoup per llegir el contingut de les dades")
 def readPage(page):
     res = []
     url = "https://www.meneame.net/?page={}".format(page)
@@ -16,27 +16,27 @@ def readPage(page):
         extern_url = article.find('h2').a['href']
         votes = article.find(attrs={'class': 'votes'}).a.string
         date1_node = article.find('span', {'data-ts': True})
-        date1 = date1_node['data-ts']
+        published_date = date1_node['data-ts']
         if date1_node.find_next_sibling('span', {'data-ts': True}):
-            date2 = date1_node.next_sibling.next_sibling['data-ts']
+            send_date = date1_node.next_sibling.next_sibling['data-ts']
         else:
-            date2 = '0'
+            send_date = '0'
         category = article.find(attrs={'class': 'subname'}).string
         res.append(Entry(page, extern_url, title,
-                   summary, votes, date1, date2, category))
+                   summary, votes, published_date, send_date, category))
     return res
 
+print("Creem una funció que vagi llegin les pàgines que demanem i aconseguim els continguts de: page, url, title, summary, votes, published_date, send_date, category")
 
 class Entry:
-    def __init__(self, page, url, title, summary, votes, date1, date2, category):
+    def __init__(self, page, url, title, summary, votes, published_date, send_date, category):
         self.page = page
         self.url = url
         self.title = title
         self.summary = summary
         self.votes = votes
-        self.date1 = date1
-        self.date2 = date2
+        self.published_date = published_date
+        self.send_date = send_date
         self.category= category
         
-
-        
+print("Vinculem els atributs amb els arguments que l'hi hem donat")
